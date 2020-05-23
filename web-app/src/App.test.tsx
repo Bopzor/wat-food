5a163e1a7988bf8e76f0695a2088764803415a5f
Bopzor <violaine.ballandin@gmail.com>
2020-05-23 23:49:38 +0200

    chore: app shell

---
+++ web-app/src/App.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import App from './App';
+
+describe('App', () => {
+
+  it('should render App', () => {
+    const { getByText } = render(<App />)
+
+    expect(getByText('App!')).toBeTruthy();
+  });
+
+});
