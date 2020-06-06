a6a14db05cae79afb21399c8c90b82c24f392fb6
Bopzor <violaine.ballandin@gmail.com>
2020-06-07 01:12:57 +0200

    chore(api): setup jest supertest

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,9 +2,13 @@ import * as express from 'express';
 
 const PORT = process.env.PORT || 3000;
 
-const app = express();
+export const app = express();
 
 app.use(express.json());
+
+app.get('/', (req, res, next) => {
+  res.status(200).send('hello');
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on ${PORT}`);
